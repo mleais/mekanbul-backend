9095ec5d58cbed4f995cdaecbb1ec7653c77cb12
mleais <oztasmelisa1864@gmail.com>
2025-12-10 14:49:04 +0300

    db dosyasi kesin duzeltildi

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,16 +1,28 @@
 var mongoose = require("mongoose");
 var dbURI = "mongodb://localhost/mekanbul";
 
-// VERCEL İÇİN GEREKLİ AYAR
+// BU KISIM EKSİK OLDUĞU İÇİN ÇALIŞMIYOR:
 if (process.env.NODE_ENV === "production") {
     dbURI = process.env.MONGODB_URI;
 }
 
 mongoose.connect(dbURI);
 
-// Bağlantı mesajları...
 mongoose.connection.on("connected", function() {
     console.log("Mongoose " + dbURI + " adresine baglandi.");
 });
-// ... diğer kodlar aynı kalabilir ...
+mongoose.connection.on("error", function(err) {
+    console.log("Mongoose baglanti hatasi: " + err);
+});
+mongoose.connection.on("disconnected", function() {
+    console.log("Mongoose baglantisi kesildi.");
+});
+
+process.on("SIGINT", function() {
+    mongoose.connection.close(function() {
+        console.log("Mongoose baglantisi kapatildi.");
+        process.exit(0);
+    });
+});
+
 require("./venue");
