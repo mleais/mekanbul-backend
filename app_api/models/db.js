ef9c84221b98a2565d9670e7999670ff5e4fbebf
mleais <oztasmelisa1864@gmail.com>
2025-12-10 13:54:21 +0300

    db.js vercel icin duzeltildi

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,31 +1,32 @@
 var mongoose = require("mongoose");
 
-// Varsayılan yerel bağlantı
+// Varsayılan yerel bağlantı adresi
 var dbURI = "mongodb://localhost/mekanbul";
 
 // EĞER VERCEL'DEYSEK (Production), CLOUD ADRESİNİ KULLAN
-// Bu kısım olmadan projeniz Vercel'de çalışmaz!
 if (process.env.NODE_ENV === "production") {
     dbURI = process.env.MONGODB_URI;
 }
 
 mongoose.connect(dbURI);
 
-// Bağlantı mesajları
+// Bağlantı olaylarını dinle
 mongoose.connection.on("connected", function() {
     console.log("Mongoose " + dbURI + " adresine baglandi.");
 });
+
 mongoose.connection.on("error", function(err) {
     console.log("Mongoose baglanti hatasi: " + err);
 });
+
 mongoose.connection.on("disconnected", function() {
     console.log("Mongoose baglantisi kesildi.");
 });
 
-// Uygulama kapandığında
+// Uygulama kapandığında bağlantıyı kapat
 process.on("SIGINT", function() {
     mongoose.connection.close(function() {
-        console.log("Mongoose baglantisi kapatildi.");
+        console.log("Mongoose baglantisi uygulama sonlandigi icin kapatildi.");
         process.exit(0);
     });
 });
