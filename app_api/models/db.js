f6b39051004da89c9eab58e5066e92241cbadd2c
mleais <oztasmelisa1864@gmail.com>
2025-12-10 14:29:44 +0300

    controller ismi ve db ayari duzeltildi

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,34 +1,16 @@
 var mongoose = require("mongoose");
-
-// Varsayılan yerel bağlantı adresi
 var dbURI = "mongodb://localhost/mekanbul";
 
-// EĞER VERCEL'DEYSEK (Production), CLOUD ADRESİNİ KULLAN
+// VERCEL İÇİN GEREKLİ AYAR
 if (process.env.NODE_ENV === "production") {
     dbURI = process.env.MONGODB_URI;
 }
 
 mongoose.connect(dbURI);
 
-// Bağlantı olaylarını dinle
+// Bağlantı mesajları...
 mongoose.connection.on("connected", function() {
     console.log("Mongoose " + dbURI + " adresine baglandi.");
 });
-
-mongoose.connection.on("error", function(err) {
-    console.log("Mongoose baglanti hatasi: " + err);
-});
-
-mongoose.connection.on("disconnected", function() {
-    console.log("Mongoose baglantisi kesildi.");
-});
-
-// Uygulama kapandığında bağlantıyı kapat
-process.on("SIGINT", function() {
-    mongoose.connection.close(function() {
-        console.log("Mongoose baglantisi uygulama sonlandigi icin kapatildi.");
-        process.exit(0);
-    });
-});
-
+// ... diğer kodlar aynı kalabilir ...
 require("./venue");
